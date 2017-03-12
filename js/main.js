9c04464a29ea10fee308511da3f820b00e829086
Kelsey Legault <kelsey.legault@gmail.com>
2017-03-11 21:16:30 -0800

    Make animations smoother

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 console.log("meossw");
 var width = $(".ad").width();
-$(".ad__popover").width(width);
+$(".ad").width(width);
 $('.frame-item').click(function(){
     $(this).addClass("frame-item__hide");
     console.log("click");
-});
-
-$('.ad').hover(function(){
-    console.log("hover");
 });

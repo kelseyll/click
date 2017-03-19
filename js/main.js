af9946cdf4ad2748c8639e37549f32ab2af99052
Kelsey Legault <kelsey.legault@gmail.com>
2017-03-19 15:08:22 -0700

    Change to single column, no scroll

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-// Main controller for CSC 497 Project
-// Kelsey Legault - Jan to April 2017
+$(document).ready(function () {
 
-// Get width of content for animation.
-var width = $(".ad").width();
-$(".ad").width(width);
+    // Main controller for CSC 497 Project
+    // Kelsey Legault - Jan to April 2017
 
-// Add animation on click.
-$('.frame-item').click(function(){
-    $(this).addClass("frame-item__hide");
-    console.log("click");
+    // Get width of content for animation.
+    var width = $(".ad").width();
+    $(".ad").width(width);
+
+    $( ".frame-item" ).each(function( index ) {
+        var height = $(this).height();
+        $(this).height(height);
+    });
+    // Add animation on click.
+    $('.frame-item').click(function(){
+        $(this).addClass("frame-item__hide");
+        var nextitem = $(this).next();
+        nextitem.addClass("frame-item__dunclick");
+        setTimeout(function(){
+          nextitem.removeClass("frame-item__dunclick")
+        }, 500);
+        console.log("click");
+    });
 });

ef665df8945110d6e1eff370d02ad70c96b0f91d
Kelsey Legault <kelsey.legault@gmail.com>
2017-03-19 13:56:02 -0700

    Add some documentation notes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-console.log("meossw");
+// Main controller for CSC 497 Project
+// Kelsey Legault - Jan to April 2017
+
+// Get width of content for animation.
 var width = $(".ad").width();
 $(".ad").width(width);
+
+// Add animation on click.
 $('.frame-item').click(function(){
     $(this).addClass("frame-item__hide");
     console.log("click");

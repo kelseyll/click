a85aa3ae0bb6ace4898e552a3b00068c7f3a01d9
Kelsey Legault <kelsey.legault@gmail.com>
2017-04-08 17:24:53 -0700

    Add description modal functionality

--- js/main.js
+++ js/main.js
@@ -14,17 +14,42 @@ $(document).ready(function () {
         setTimeout(function(){
           nextitem.removeClass("frame-item__dunclick")
         }, 500);
-        console.log("click");
     });
 
     // Timer
-    var sec = 60
+    var sec = 59;
     var timer = setInterval(function() {
-        $('.timer').text(sec--);
+        if (sec > 9) {
+        $('.timer').text("0:" + sec--);
         // When timer runs out
+    }
+        if (sec <= 9) {
+            $('.timer').text("0:0" + sec--);
+        }
         if (sec == -1) {
             // Stop timer
             clearInterval(timer);
+            $('.timer').text("0:00");
         }
     }, 1000);
+
+    // Score
+    var score = 0;
+    $(".like").click(function(){
+        score = score + 1;
+        $('.score').text(score);
+    });
+    $(".heart").click(function(){
+        score = score + 2;
+        console.log("click");
+        $('.score').text(score);
+    });
+
+    // Modal
+    $(".description").click(function(){
+        $(".modal-container").toggle();
+        $(".modal.description").toggle();
+
+        // $(this).find("span").text(XX);
+    });
 });

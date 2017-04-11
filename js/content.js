0764d5a3100936890718f1e81a473eafdd666ee8
Kelsey Legault <kelsey.legault@gmail.com>
2017-04-10 20:13:43 -0700

    Test link array

--- js/content.js
+++ js/content.js
@@ -1,2 +1,4 @@
 var content_list =
-    ["Saab", "Volvo", "BMW"];
+    ["https://media.giphy.com/media/13gvXfEVlxQjDO/giphy.gif",
+     "https://media.giphy.com/media/l2JJrEx9aRsjNruhi/giphy.gif",
+     "https://media.giphy.com/media/l0HlRyxGrLxUXq5KE/giphy.gif"];
